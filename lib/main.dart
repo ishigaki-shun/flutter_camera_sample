3e12d054a4be5a56ea855e5024a549f0950e69a6
石垣駿 <ishigakishun@ishigakishun-no-MacBook-Pro.local>
2018-08-13 21:58:08 +0900

    カメラの撮影機能追加

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,11 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:video_player/video_player.dart';
 
 List<CameraDescription> cameras;
 
+// 実行されるmain関数
 Future<Null> main() async {
-  // Fetch the available cameras before initializing the app.
   try {
     cameras = await availableCameras();
   } on CameraException catch (e) {}
@@ -17,44 +16,213 @@ Future<Null> main() async {
   runApp(new MyApp());
 }
 
+// Stateを持たないWidgetオブジェクト
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
+// Stateを持つWidgetオブジェクト
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
+// StatefulWidgetで管理されるStateオブジェクト
 class _MyHomePageState extends State<MyHomePage> {
+  CameraController controller;
+  String imagePath;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: _scaffoldKey,
       appBar: new AppBar(
-        title: new Text(widget.title),
+        title: const Text('Camera example'),
       ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+      body: new Column(
+        children: <Widget>[
+          new Expanded(
+            child: new Container(
+              child: new Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: new Center(
+                  child: _cameraPreviewWidget(),
+                ),
+              ),
             ),
-          ],
+          ),
+          _captureIconWidget(),
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _cameraTogglesRowWidget(),
+                _thumbnailWidget(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // カメラのプレビューWidget
+  Widget _cameraPreviewWidget() {
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Tap a camera',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    } else {
+      return new AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: new CameraPreview(controller),
+      );
+    }
+  }
+
+  // サムネイルWidget
+  Widget _thumbnailWidget() {
+    return new Expanded(
+      child: new Align(
+        alignment: Alignment.centerRight,
+        child: imagePath == null
+            ? null
+            : new SizedBox(
+          child: new Image.file(new File(imagePath)),
+          width: 64.0,
+          height: 64.0,
         ),
       ),
     );
   }
+
+  // カメラのアイコンWidget
+  Widget _captureIconWidget() {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        new IconButton(
+          icon: const Icon(Icons.camera_alt),
+          color: Colors.blue,
+          onPressed: controller != null &&
+              controller.value.isInitialized
+              ? onTakePictureButtonPressed
+              : null,
+        ),
+      ],
+    );
+  }
+
+  // カメラのインアウトを切り替えるトグルWidget
+  Widget _cameraTogglesRowWidget() {
+    final List<Widget> toggles = <Widget>[];
+
+    if (cameras.isEmpty) {
+      return const Text('No camera found');
+    } else {
+      for (CameraDescription cameraDescription in cameras) {
+        toggles.add(
+          new SizedBox(
+            width: 90.0,
+            child: new RadioListTile<CameraDescription>(
+              title:
+              new Icon(getCameraLensIcon(cameraDescription.lensDirection)),
+              groupValue: controller?.description,
+              value: cameraDescription,
+              onChanged: controller != null
+                  ? null
+                  : onNewCameraSelected,
+            ),
+          ),
+        );
+      }
+    }
+
+    return new Row(children: toggles);
+  }
+
+  // トグルが選択された時に呼ばれるリスナ
+  void onNewCameraSelected(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = new CameraController(cameraDescription, ResolutionPreset.high);
+
+    controller.addListener(() {
+      if (mounted) setState(() {});
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {}
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  // カメラアイコンが押された時に呼ばれるリスナ
+  void onTakePictureButtonPressed() {
+    takePicture().then((String filePath) {
+      if (mounted) {
+        setState(() {
+          imagePath = filePath;
+        });
+      }
+    });
+  }
+
+  // タイムスタンプを返す関数
+  String timestamp() => new DateTime.now().millisecondsSinceEpoch.toString();
+
+  // カメラで撮影した画像を保存する関数(非同期)
+  Future<String> takePicture() async {
+    if (!controller.value.isInitialized) {
+      return null;
+    }
+
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/Pictures/flutter_test';
+    await new Directory(dirPath).create(recursive: true);
+    final String filePath = '$dirPath/${timestamp()}.jpg';
+
+    if (controller.value.isTakingPicture) {
+      return null;
+    }
+
+    try {
+      await controller.takePicture(filePath);
+    } on CameraException catch (e) {
+      return null;
+    }
+
+    return filePath;
+  }
+}
+
+// カメラのアイコン画像を返す関数
+IconData getCameraLensIcon(CameraLensDirection direction) {
+  switch (direction) {
+    case CameraLensDirection.back:
+      return Icons.camera_rear;
+    case CameraLensDirection.front:
+      return Icons.camera_front;
+    case CameraLensDirection.external:
+      return Icons.camera;
+  }
+  throw new ArgumentError('Unknown lens direction');
 }
